f2d755052a7bc120fe1562ce2cb333f173e51b19
automateLinux <root@localhost>
2026-03-24 10:21:16 +0200

    #1: remove Hi! text from homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-        <FeedbackChat />
+        <FeedbackChat issuesPath="/issues" />
       </body>
     </html>
   );
